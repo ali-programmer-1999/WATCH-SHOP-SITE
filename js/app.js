48508e43f681962fdd289d3df6ab0a7f662c266e
Alirg <ali.programer.1999@gmail.com>
2022-02-08 22:27:12 +0330

    finish change home bar

--- js/app.js
+++ js/app.js
@@ -13,4 +13,44 @@ setInterval(function(){
         current = 0;
         slides.style.left = 0;
     }
-},7500)
+},7500);
+// ==============================
+var slid = document.getElementsByClassName("side");
+var prev = document.querySelector('.prev');
+var next = document.querySelector('.next');
+var n = 0;
+var i; 
+
+function disnone(){
+    for( i = 0 ; i<slid.length ; i++){
+        slid[i].style.display="none";
+    }
+}
+
+next.addEventListener('click',function(e){
+    e.preventDefault();
+    n++;
+    if(n > slid.length-1){
+        n= 0;
+    }
+    disnone();
+    slid[n].style.display ='block';
+})
+prev.addEventListener('click',function(e){
+    e.preventDefault();
+    n--;
+    if(n < 0){
+        n= slid.length-1;
+    }
+    disnone();
+    slid[n].style.display ='block';
+})
+
+setInterval(() => {
+    n++;
+    if(n > slid.length-1){
+        n= 0;
+    }
+    disnone();
+    slid[n].style.display ='block';
+}, 5000);
